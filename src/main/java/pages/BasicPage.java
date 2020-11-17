9eee9676e5f6d9964ae60a526b5e8b80ea207b2f
Catea13 <58842293+Catea13@users.noreply.github.com>
2020-11-17 11:37:50 +0200

    Update BasicPage.java

--- src/main/java/pages/BasicPage.java
+++ src/main/java/pages/BasicPage.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.Condition;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
-
+/**  In this class  I am looking for all elements BY Xpath*/
 public class BasicPage {
 
     public void clickButton(String text) {
