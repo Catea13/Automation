25bc4053af5da6405030da7cb8b1662185cb647a
Catea13 <58842293+Catea13@users.noreply.github.com>
2020-11-17 11:42:26 +0200

    Update Hooks.java

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -3,9 +3,9 @@ package steps;
 import io.cucumber.java.Before;
 
 import static com.codeborne.selenide.Selenide.*;
-
+/***  in this class the very first actions will be triggered*/
 public class Hooks {
-
+/*** Method openUrl- opened site  */
     @Before
     public void openUrl() {
         open("http://kia-moldova.md/ru/");
